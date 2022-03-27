970e78787ff1f4c83a16af2a8188192e4c0132b0
fafa <fafa@wrmsoftware.com>
2022-03-27 22:37:38 +0700

    error input =0

--- Kata/01-FizzBuzz/FissBuzz.cs
+++ Kata/01-FizzBuzz/FissBuzz.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Kata.FissBuzz;
 
 public class FissBuzz
@@ -5,10 +7,24 @@ public class FissBuzz
 
     public string GetFissBuzz(int input)
     {
-        if ((input % 5 == 0) &&(input % 3 == 0)) return "FizzBuzz";
+        if ((input % 5 == 0) && (input % 3 == 0)) return "FizzBuzz";
         if (input % 5 == 0) return "Buzz";
         if (input % 3 == 0) return "Fizz";
         return input.ToString();
+    }
+    public  string PrintFizzBuzz(int number)
+    {
+        CheckWhenNumberNotInRule(number);
+      return GetFissBuzz(number);
+    }
+    public string CheckWhenNumberNotInRule(int number) 
+    {
+        if (number > 100 || number < 1)
+            throw new ArgumentException(
+                string.Format(
+                    "entered number is [{0}], which does not meet rule, entered number should be between 1 to 100.",
+                    number));
+        return string.Empty;
     }
 }
 
