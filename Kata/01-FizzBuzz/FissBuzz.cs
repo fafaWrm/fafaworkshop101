59139d18afaaba070089ecf5dfc8aa544e2e09bf
fafa <fafa@wrmsoftware.com>
2022-03-27 23:32:40 +0700

    Dessign semantic naming conventions And Clean code

--- Kata/01-FizzBuzz/FissBuzz.cs
+++ Kata/01-FizzBuzz/FissBuzz.cs
@@ -5,6 +5,10 @@ namespace Kata.FissBuzz;
 
 public class FissBuzz
 {
+    
+    public const string buzz = "Buzz";
+    public const string fizz = "Fizz";
+    public readonly string argumentExceptioNumberNotInRule = "entered number is [{0}], which does not meet rule, entered number should be between 1 to 100.";
 
 
     public string PrintFizzBuzz()
@@ -21,30 +25,42 @@ public class FissBuzz
         if (number > 100 || number < 1)
             throw new ArgumentException(
                 string.Format(
-                    "entered number is [{0}], which does not meet rule, entered number should be between 1 to 100.",
+                   argumentExceptioNumberNotInRule,
                     number));
         return string.Empty;
     }
     public string GetFissBuzz(int input)
     {
-        if ((input % 5 == 0) && (input % 3 == 0)) return "FizzBuzz";
-        if (input % 5 == 0) return "Buzz";
-        if (input % 3 == 0) return "Fizz";
-        return input.ToString();
+        string resultFizzBuzz = string.Empty;       
+        if (IsFizz(input)) 
+                resultFizzBuzz += fizz;
+        if (IsBuzz(input))
+                resultFizzBuzz += buzz;
+        if (string.IsNullOrEmpty(resultFizzBuzz))
+            resultFizzBuzz += input.ToString();
+
+       return resultFizzBuzz;
     }
     public string GetFissBuzz()
     {
         string resultFizzBuzz = string.Empty;
-        for (var i = 1; i <= 100; i++)        {
+        for (var i = 1; i <= 100; i++)
+        {
             var printNumber = string.Empty;
-             printNumber += GetFissBuzz(i);                  
-          
+            printNumber += GetFissBuzz(i);
+
             if (string.IsNullOrEmpty(printNumber))
                 printNumber = i.ToString(CultureInfo.InvariantCulture);
-            
+
             resultFizzBuzz += " " + printNumber;
         }
         return resultFizzBuzz.Trim();
     }
+
+    private bool IsBuzz(int i) => i % 5 == 0;
+
+    private bool IsFizz(int i) => i % 3 == 0;
+
+
 }
 
