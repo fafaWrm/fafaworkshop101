0274d1a3e837abf3b6896aadd65bf5a8a64db510
fafa <fafa@wrmsoftware.com>
2022-03-27 23:05:01 +0700

    error กรณีเริ่มับจาก 1-100

--- Kata.Tests/01-FizzBuzz/FizzBuzzTests.cs
+++ Kata.Tests/01-FizzBuzz/FizzBuzzTests.cs
@@ -3,7 +3,7 @@ using System;
 namespace Kata.FissBuzz.Tests;
 public class FizzBuzzTests
 {
-    private string output = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz";
+    private string output = @"1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz"; 
     [Theory]
     [InlineData(1, "1")]
     [InlineData(2, "2")]
@@ -11,7 +11,7 @@ public class FizzBuzzTests
     [InlineData(5, "Buzz")]
     [InlineData(15, "FizzBuzz")]
     [InlineData(100, "Buzz")]
-    public void TestFissBuzz(int input, string expected)
+    public void TestFissSingleNumber(int input, string expected)
     {
         // Arrange
         var fissBuzz = new FissBuzz();
@@ -24,7 +24,7 @@ public class FizzBuzzTests
     [Theory]
     [InlineData(0)]
     [InlineData(-1)]
-    [InlineData(101)]
+    [InlineData(101)]    
     public void TestFissBuzzThrowArgumentException(int input)
     {
         // Arrange
@@ -36,7 +36,17 @@ public class FizzBuzzTests
             string.Format(
                 "entered number is [{0}], which does not meet rule, entered number should be between 1 to 100.",
                 input.ToString()));
+    }
 
+    [Fact]
+    public void TestFissBuzz()
+    {
+        // Arrange
+        var fissBuzz = new FissBuzz();
+        // Act
+        var actFissBuzz = fissBuzz.PrintFizzBuzz();
+        //// Assert
+        actFissBuzz.Should().Be(output);
     }
 }
 
--- Kata/01-FizzBuzz/FissBuzz.cs
+++ Kata/01-FizzBuzz/FissBuzz.cs
@@ -1,23 +1,22 @@
 using System;
+using System.Globalization;
 
 namespace Kata.FissBuzz;
 
 public class FissBuzz
 {
 
-    public string GetFissBuzz(int input)
+
+    public string PrintFizzBuzz()
     {
-        if ((input % 5 == 0) && (input % 3 == 0)) return "FizzBuzz";
-        if (input % 5 == 0) return "Buzz";
-        if (input % 3 == 0) return "Fizz";
-        return input.ToString();
+        return GetFissBuzz();
     }
-    public  string PrintFizzBuzz(int number)
+    public string PrintFizzBuzz(int number)
     {
         CheckWhenNumberNotInRule(number);
-      return GetFissBuzz(number);
+        return GetFissBuzz(number);
     }
-    public string CheckWhenNumberNotInRule(int number) 
+    public string CheckWhenNumberNotInRule(int number)
     {
         if (number > 100 || number < 1)
             throw new ArgumentException(
@@ -25,6 +24,29 @@ public class FissBuzz
                     "entered number is [{0}], which does not meet rule, entered number should be between 1 to 100.",
                     number));
         return string.Empty;
+    }
+    public string GetFissBuzz(int input)
+    {
+        if ((input % 5 == 0) && (input % 3 == 0)) return "FizzBuzz";
+        if (input % 5 == 0) return "Buzz";
+        if (input % 3 == 0) return "Fizz";
+        return input.ToString();
+    }
+    public string GetFissBuzz()
+    {
+        string resultFizzBuzz = string.Empty;
+        for (var i = 1; i <= 100; i++)
+        {
+            var printNumber = string.Empty;
+            if ((i % 5 == 0) && (i % 3 == 0)) printNumber += "FizzBuzz";
+            if (i % 5 == 0) printNumber += "Buzz";
+            if (i % 3 == 0) printNumber += "Fizz";           
+            if (string.IsNullOrEmpty(printNumber))
+                printNumber = i.ToString(CultureInfo.InvariantCulture);
+          
+            resultFizzBuzz += " " + printNumber;
+        }
+        return resultFizzBuzz.Trim();
     }
 }
 
